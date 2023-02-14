f993241432b9ce2f8f7ffc9e063347cd8fd93aea
Zalman Izak <thezalmanian@gmail.com>
2023-02-14 07:40:52 -0800

    Implemented Addition method

--- CPW211-UnitTestStarterCode/SimpleMath.cs
+++ CPW211-UnitTestStarterCode/SimpleMath.cs
@@ -13,9 +13,16 @@ namespace CPW211_UnitTestStarterCode
     * ***************************************************************************/
     public static class SimpleMath
     {
+        /// <summary>
+        /// Calculates and returns the sum of the given numbers when added together
+        /// </summary>
+        /// <param name="a">The first operand</param>
+        /// <param name="b">The second operand</param>
+        /// <returns>The sum of both numbers when added together</returns>
         public static double Add(double a, double b)
         {
-            throw new NotImplementedException();
+            // Calculate and return the sum            
+            return a + b;
         }
 
         public static double Subtract(double a, double b)
